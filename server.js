8cf26aa75937dac5a9aae883eee52862624fd409
BodaMops25 <bodabodamops@gmail.com>
2022-01-22 12:39:15 +0200

    update servers

--- server.js
+++ server.js
@@ -1,6 +1,57 @@
-const {server} = require('./static-server.js'),
-      {Server} = require('ws'),
-      wss = new Server({server})
+// STATIC SERVER
+
+const http = require('http'),
+      PORT = process.env.PORT || 8080,
+      fs = require('fs').promises
+
+const server = http.createServer((req, res) => {
+
+  if(req.method === 'GET') {
+
+    if(req.url === '/') req.url = '/game.html'
+
+    fs.readFile(__dirname + req.url)
+    .then(content => {
+
+      let contentType = ''
+
+      switch(req.url.match(/\..+$/)[0]) {
+        case '.js':
+          contentType = 'text/javascript'
+          break
+        case '.css':
+          contentType = 'text/css'
+          break
+        case '.html':
+          contentType = 'text/html' 
+          break
+      }
+
+      res.writeHead(200, {'Content-Type': contentType})
+      res.write(content)
+      res.end()
+    })
+    .catch(err => {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      res.write('<meta charset="utf-8">')
+      res.write('<h1><strong>404</strong> error, page not found<h1>')
+      res.write(`<pre>${err}}</pre>`)
+      res.end()
+    })
+  }
+  else if(req.method === 'POST') {
+    res.writeHead(405, {'Content-Type': 'text/html'})
+    res.write('<meta charset="utf-8">')
+    res.write('<h1><strong>405</strong> error, method not allowed<h1>')
+    res.end()
+  }
+  
+}).listen(PORT)
+
+// SERVER
+
+const ws = require('ws'),
+      wss = new ws.Server({server})
 
 const plyr = require('./player.js'),
       gameData = require('./game.json')
@@ -49,7 +100,7 @@ const getUpdate = setInterval(() => {
   })  
 
   wss.clients.forEach(client => {
-    if(client.readyState === WebSocket.OPEN) client.send(respondToClient())
+    if(client.readyState === ws.WebSocket.OPEN) client.send(respondToClient())
   })
 
 }, 1000 / gameData.gameFPS)
@@ -77,4 +128,4 @@ wss.on('connection', webs => {
 
 })
 
-// console.log('Server server running')
+// console.log('ws server running')
